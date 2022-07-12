ad2bccf0e0ee117ea31ae4339b1c07c5d08ff1eb
emeerdink <e.b.meerdink@gmail.com>
2022-07-12 19:11:12 +0200

    restore prev commit fix

--- R/riboseqc.R
+++ R/riboseqc.R
@@ -2690,7 +2690,7 @@ prepare_annotation_files<-function(annotation_directory,twobit_file=NULL,gtf_fil
 
          gtfdata <- import.gff2(gtf_file,colnames=c("gene_id","gene_biotype","gene_type","gene_name","gene_symbol","transcript_id","transcript_biotype","transcript_type","type"))
          n_transcripts = length(unique(gtfdata$transcript_id))
-         stopifnot('transcript_id' %in% gtfdata$type)
+         stopifnot('transcript' %in% gtfdata$type)
          gtfdata <- subset(gtfdata, type=='transcript')
          gtfdata$type <- NULL
          stopifnot(length(unique(gtfdata$transcript_id))==n_transcripts)
